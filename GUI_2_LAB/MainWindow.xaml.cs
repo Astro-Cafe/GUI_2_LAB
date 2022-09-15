43290d80aaa34da682d6bbc71bd5d949e42a87c4
Kecskeméti.Levente <kiskecsoka@gmail.com>
2022-09-15 12:55:08 +0200

    Add project files.

---
+++ GUI_2_LAB/MainWindow.xaml.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GUI_2_LAB
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void Window_Loaded(object sender, RoutedEventArgs e)
+		{
+			List<Quiz> quizList = File.ReadAllLines("example.txt")
+				.Select(x => new Quiz(x.Split(":")[0], x.Split(":")[1], new string[] { x.Split(":")[1], x.Split(":")[2], x.Split(":")[3], x.Split(":")[4] })).ToList();
+			
+
+			int i = 0;
+			quizList.ForEach(quiz =>
+			{
+				Label l = new Label();
+				l.Tag = quiz;
+				l.Content = i++;
+				l.HorizontalContentAlignment = HorizontalAlignment.Center;
+				l.VerticalContentAlignment = VerticalAlignment.Center;
+				l.Foreground = Brushes.Gainsboro;
+				l.Background = new SolidColorBrush(Color.FromRgb(54, 57, 62));
+				l.Margin = new Thickness(10);
+				l.Height = this.ActualHeight / 3;
+				l.Width = this.ActualWidth / 6;
+				l.FontSize = 36;
+				wrap.Children.Add(l);
+
+				l.MouseLeftButtonDown += L_MouseLeftButtonDown;
+				l.MouseMove += L_MouseMove;
+				l.MouseLeave += L_MouseLeave;
+			});
+		}
+
+		private void L_MouseLeave(object sender, MouseEventArgs e)
+		{
+			if (e.Source is Label)
+			{
+				(e.Source as Label).Background = new SolidColorBrush(Color.FromRgb(54, 57, 62));
+			}
+		}
+
+		private void L_MouseMove(object sender, MouseEventArgs e)
+		{
+			if (e.Source is Label)
+			{
+				(e.Source as Label).Background = new SolidColorBrush(Color.FromRgb(66, 69, 73));
+			}
+		}
+
+		private void L_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+		{
+			Quiz q = (Quiz)(sender as Label).Tag;
+
+			QuizWindow qw = new QuizWindow(q);
+			if (qw.ShowDialog()==true)
+			{
+				(sender as Label).Background = Brushes.LightGreen;
+				(sender as Label).IsEnabled = false;
+				//tb_correct.Text = (int.Parse(tb_correct.Text)+1).ToString();
+			}
+			else
+			{
+				(sender as Label).Background = Brushes.DarkRed;
+				(sender as Label).IsEnabled = false;
+				//tb_incorrect.Text = (int.Parse(tb_incorrect.Text) + 1).ToString();
+			}
+			
+		}
+	}
+
+	public class Quiz
+	{
+		private string question;
+		private string answer;
+		private string[] options;
+		public Quiz(string question,string answer,string[] option)
+		{
+			this.question = question;
+			this.answer = answer;
+			this.options = option;
+			options.Shuffle<string>();
+		}
+
+		public string Question { get => question; set => question = value; }
+		public string Answer { get => answer; set => answer = value; }
+		public string[] Options { get => options; set => options = value; }
+	}
+
+	public static class ShuffleExtenstions
+	{
+		private static Random rng = new Random();
+		public static void Shuffle<T>(this T[] array)
+		{
+			rng = new Random();
+			int n = array.Length;
+			while (n > 1)
+			{
+				int k = rng.Next(n);
+				n--;
+				T temp = array[n];
+				array[n] = array[k];
+				array[k] = temp;
+			}
+		}
+	}
+}
---
+++ GUI_2_LAB/QuizWindow.xaml.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace GUI_2_LAB
+{
+	/// <summary>
+	/// Interaction logic for QuizWindow.xaml
+	/// </summary>
+	public partial class QuizWindow : Window
+	{
+		DispatcherTimer _timer;
+		TimeSpan _time;
+		private Quiz q;
+		private string selected;
+		public QuizWindow(Quiz q)
+		{
+			InitializeComponent();
+
+			this.q = q;
+
+			int i = 0;
+			tb_question.Text = q.Question;
+			foreach (var item in q.Options)
+			{
+				(grid_Answers.Children[i] as Label).Content += item;
+				i++;
+			}
+		}
+
+		private void bt_submit_Click(object sender, RoutedEventArgs e)
+		{
+			
+			if (selected==q.Answer)
+			{
+				this.DialogResult = true;
+			}
+			else
+			{
+				this.DialogResult = false;
+			}
+		}
+
+		private void bt_submit_MouseEnter(object sender, MouseEventArgs e)
+		{
+			this.bt_submit.Background = new SolidColorBrush(Color.FromRgb(66, 69, 73));
+		}
+
+		private void bt_submit_MouseLeave(object sender, MouseEventArgs e)
+		{
+			this.bt_submit.Background = new SolidColorBrush(Color.FromRgb(54, 57, 62));
+		}
+
+		private void l_MouseEnter(object sender, MouseEventArgs e)
+		{
+			if ((e.Source as Label).IsEnabled == true)
+			{
+				(e.Source as Label).Background = new SolidColorBrush(Color.FromRgb(66, 69, 73));
+			}
+		}
+		private void l_MouseLeave(object sender, MouseEventArgs e)
+		{
+			if ((e.Source as Label).IsEnabled==true)
+			{
+				(e.Source as Label).Background = new SolidColorBrush(Color.FromRgb(54, 57, 62));
+			}
+		}
+
+		private void l_MouseClick(object sender, MouseButtonEventArgs e)
+		{
+			foreach (var item in grid_Answers.Children)
+			{
+				(item as Label).IsEnabled = true;
+				(item as Label).Background = new SolidColorBrush(Color.FromRgb(54, 57, 62));
+			}
+
+			(e.Source as Label).IsEnabled = false;
+
+			selected = (e.Source as Label).Content.ToString().Split(": ")[1];
+			(e.Source as Label).Background = new SolidColorBrush(Color.FromRgb(66, 69, 73));
+		}
+
+		private void Window_Loaded(object sender, RoutedEventArgs e)
+		{
+
+				_time = TimeSpan.FromSeconds(20);
+
+				_timer = new DispatcherTimer(new TimeSpan(0, 0, 1), DispatcherPriority.Normal, delegate
+				{
+					tb_timer.Text = _time.TotalSeconds.ToString();
+					if (_time == TimeSpan.Zero)
+					{
+						_timer.Stop();
+						this.DialogResult = false;
+					}
+					if (_time==TimeSpan.FromSeconds(10))
+					{
+						tb_timer.Background = Brushes.DarkRed;
+					}
+					_time = _time.Add(TimeSpan.FromSeconds(-1));
+				}, Application.Current.Dispatcher);
+
+				_timer.Start();
+
+		}
+
+		private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+		{
+			MessageBoxResult closing = MessageBox.Show("Biztos vagy benne? Nincs visszalépés.", "Kérdés", MessageBoxButton.YesNo);
+
+			if (closing == MessageBoxResult.No)
+			{
+				e.Cancel = true;
+			}
+			else
+			{
+				_timer.Stop();
+			}
+		}
+	}
+}
